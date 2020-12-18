ac2fb3846a2de846547c653852e2c5bd7ca1bc01
PaulaScharf <paulascharf@gmx.de>
2020-12-18 16:27:33 +0100

    added option to use prerecorded videos

--- real_time_object_detection.py
+++ real_time_object_detection.py
@@ -2,7 +2,6 @@
 import os, time, datetime, webbrowser
 from counting_cars import Detection, analyseDectectionData, belongsToBboxes
 from imutils.video import VideoStream
-from imutils.video import FPS
 import numpy as np
 import argparse
 import imutils
@@ -37,77 +36,96 @@ net = cv2.dnn.readNetFromCaffe(basePath + "/models/MobileNetSSD/MobileNetSSD_dep
 ##net = cv2.dnn.readNetFromCaffe(basePath + "/models/googlenet_cars/MobileNetSSD_deploy.prototxt.txt",
 ##        basePath + "/models/googlenet_cars/googlenet_finetune_web_car_iter_10000.caffemodel")
 
-# initialize the video stream, allow the cammera sensor to warmup,
-# and initialize the FPS counter
-print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
-fps = FPS().start()
-
 collectedDetections = []
-counter = 1
 
-# loop over the frames from the video stream
-while True:
-    # grab the frame from the threaded video stream and resize it
-    # to have a maximum width of 400 pixels
-    frame = vs.read()
-    frame = imutils.resize(frame, width=1000)
-    # grab the frame dimensions and convert it to a blob
-    (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
-		0.007843, (300, 300), 127.5)
-	# pass the blob through the network and obtain the detections and
-	# predictions
-    net.setInput(blob)
-    detections = net.forward()
-    # loop over the detections
-    for i in np.arange(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with
-        # the prediction
-        confidence = detections[0, 0, i, 2]
-        idx = int(detections[0, 0, i, 1])
-        name = CLASSES[idx]
-        # filter out weak detections by ensuring the `confidence` is
-        # greater than the minimum confidence
-        if name == "car" and confidence > args["confidence"]:
-            # extract the index of the class label from the
-            # `detections`, then compute the (x, y)-coordinates of
-            # the bounding box for the object
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
 
-            # check if the car was already detected before
-            millis = int(round(time.time() * 1000))
-            #collectedDetections.append(json.dumps([str(confidence), millis, str(startX), str(startY), str(endX), str(endY)]))
-            currCounter = None
-            currEleemtn = [confidence, millis, startX, startY, endX, endY]
-            belongingIndex = belongsToBboxes(collectedDetections, currEleemtn)
-            if belongingIndex == -1:
-                collectedDetections.append([counter, currEleemtn])
-                counter = counter + 1
-                currCounter = counter
-            else:
-                collectedDetections[belongingIndex].append(currEleemtn)
-                currCounter = collectedDetections[belongingIndex][0]
-            # draw the prediction on the frame
-            label = "{}: {:.2f}%".format(CLASSES[idx],confidence * 100) + " " + str(currCounter)
-            cv2.rectangle(frame, (startX, startY), (endX, endY),COLORS[idx], 2)
-            y = startY - 15 if startY - 15 > 15 else startY + 15
-            cv2.putText(frame, label, (startX, y),cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
-            print("car!")
-		# show the output frame
-    cv2.imshow("Frame", frame)
-    key = cv2.waitKey(1) & 0xFF
-    # if the `q` key was pressed, break from the loop
-    if key == ord("q"):
-        break
-	# update the FPS counter
-    fps.update()
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# stop_condition: determines, when the analysis is halted (eg when the videostream is over)
+# vs: the videostream (could be live from camera or from prerecorded)
+# frame_skip: the amount of frames that are skipped from the video stream (to counter the lag)
+def analyze_video(stop_condition,vs,frame_skip):
+    counter = 1
+    # counts the frames
+    frame_counter = 0
+    # loop over the frames from the video stream
+    while stop_condition:
+        # grab the frame from the threaded video stream and resize it
+        # to have a maximum width of 400 pixels
+        ret, frame = vs.read()
+        if not ret:
+            print("[ERROR] There was an error reading the frame. Its value is:")
+            print(frame)
+            break
+        if frame_counter > frame_skip - 1:
+            cv2.imshow("Frame", frame)
+            frame = imutils.resize(frame, width=1000)
+            # grab the frame dimensions and convert it to a blob
+            (h, w) = frame.shape[:2]
+            blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+        		0.007843, (300, 300), 127.5)
+        	# pass the blob through the network and obtain the detections and
+        	# predictions
+            net.setInput(blob)
+            detections = net.forward()
+            # loop over the detections
+            for i in np.arange(0, detections.shape[2]):
+                # extract the confidence (i.e., probability) associated with
+                # the prediction
+                confidence = detections[0, 0, i, 2]
+                idx = int(detections[0, 0, i, 1])
+                name = CLASSES[idx]
+                # filter out weak detections by ensuring the `confidence` is
+                # greater than the minimum confidence
+                if name == "car" and confidence > args["confidence"]:
+                    # extract the index of the class label from the
+                    # `detections`, then compute the (x, y)-coordinates of
+                    # the bounding box for the object
+                    box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                    (startX, startY, endX, endY) = box.astype("int")
+        
+                    # check if the car was already detected before
+                    millis = int(round(time.time() * 1000))
+                    #collectedDetections.append(json.dumps([str(confidence), millis, str(startX), str(startY), str(endX), str(endY)]))
+                    currCounter = None
+                    currEleemtn = [confidence, millis, startX, startY, endX, endY]
+                    belongingIndex = belongsToBboxes(collectedDetections, currEleemtn)
+                    if belongingIndex == -1:
+                        collectedDetections.append([counter, currEleemtn])
+                        counter = counter + 1
+                        currCounter = counter
+                    else:
+                        collectedDetections[belongingIndex].append(currEleemtn)
+                        currCounter = collectedDetections[belongingIndex][0]
+                    # draw the prediction on the frame
+                    label = "{}: {:.2f}%".format(CLASSES[idx],confidence * 100) + " " + str(currCounter)
+                    cv2.rectangle(frame, (startX, startY), (endX, endY),COLORS[idx], 2)
+                    y = startY - 15 if startY - 15 > 15 else startY + 15
+                    cv2.putText(frame, label, (startX, y),cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+                    print("car!")
+        		# show the output frame
+            cv2.imshow("Frame", frame)
+            frame_counter = 0
+            continue
+        frame_counter += 1
+        key = cv2.waitKey(1) & 0xFF
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+        
+# true when using connected camera, false when using prerecorded mp4
+live = False
+if live:
+    # initialize the video stream, allow the cammera sensor to warmup
+    print("[INFO] starting video stream...")
+    temp_vs = VideoStream(src=0).start()
+    analyze_video(True,temp_vs, 0)
+    time.sleep(2.0)
+else:
+    print("[INFO] starting prerecorded video...")
+    temp_vs = cv2.VideoCapture(basePath + '/out2.mp4')
+    analyze_video(temp_vs.isOpened,temp_vs, 5)  
+    
+     
 # save collected data to csv
 output_file_path = "visualization/output.csv"
 addCounter = 0
@@ -129,4 +147,4 @@ with open(output_file_path, 'a') as f:
     webbrowser.open('file://' + os.path.realpath("visualization/index.html"), new=2)
 # do a bit of cleanup
 cv2.destroyAllWindows()
-vs.stop()
+temp_vs.stop()
